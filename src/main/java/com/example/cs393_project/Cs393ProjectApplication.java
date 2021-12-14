3fa477a406e7bfbd78ca4e71a86fe1fbd75fdf57
musaberkay <musa.kocabasoglu@ozu.edu.tr>
2021-12-14 23:08:07 +0300

    Spring project created.

---
+++ src/main/java/com/example/cs393_project/Cs393ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs393_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs393ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs393ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cs393_project/Cs393ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs393_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs393ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
